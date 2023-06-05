08bb62f45c789e5d0e05557f2d2157f27b41f4ac
Mohammed Aslam <120547492+iamaslammd@users.noreply.github.com>
2023-06-05 12:04:57 +0530

    New commit

---
+++ OOP/Eg1.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+tom = Turtle()
+print(tom)
+tom.shape("turtle")
+tom.color("green")
+tom.forward(100)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
